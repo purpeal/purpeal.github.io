5cd6dca317e090075385196f4fcc697957fad087
Dominik <dominik.wanatowski@lis-chengdu.com>
2025-04-17 22:21:48 +0800

    added some logic and css styling

--- docs/script.js
+++ docs/script.js
@@ -4,3 +4,9 @@ DATABASE_URL = "https://isdgdqkrijlkeqwkwhvo.supabase.co";
 anonkey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzZGdkcWtyaWpsa2Vxd2t3aHZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ4MDYwMDQsImV4cCI6MjA2MDM4MjAwNH0.SecJNiTYnXc6U-lo3yOCXtEECV_F4tTgDrqOr8XTX9U";
 const supabase = createClient(DATABASE_URL, anonkey);
+
+async function fetchData() {
+  const { data, error } = await supabase.from("People").select();
+  console.log("Fetched data", data);
+}
+document.querySelector("#submit").addEventListener("click", fetchData);
