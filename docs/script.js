1aefb1e6a8118c0cfecd5ca992dc67b4fa23d43e
Dominik <dominik.wanatowski@lis-chengdu.com>
2025-04-17 22:27:25 +0800

    changed supabase const

--- docs/script.js
+++ docs/script.js
@@ -1,9 +1,9 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-DATABASE_URL = "https://isdgdqkrijlkeqwkwhvo.supabase.co";
-anonkey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzZGdkcWtyaWpsa2Vxd2t3aHZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ4MDYwMDQsImV4cCI6MjA2MDM4MjAwNH0.SecJNiTYnXc6U-lo3yOCXtEECV_F4tTgDrqOr8XTX9U";
-const supabase = createClient(DATABASE_URL, anonkey);
+const supabase = createClient(
+  "https://isdgdqkrijlkeqwkwhvo.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzZGdkcWtyaWpsa2Vxd2t3aHZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ4MDYwMDQsImV4cCI6MjA2MDM4MjAwNH0.SecJNiTYnXc6U-lo3yOCXtEECV_F4tTgDrqOr8XTX9U"
+);
 
 async function fetchData() {
   const { data, error } = await supabase.from("People").select();
